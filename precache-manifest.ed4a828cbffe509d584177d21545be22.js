c5af9f166caeb4f48f8fa002e43eb4e8125dd061
Joseph Koon <josephandrewkoon@gmail.com>
2020-07-09 17:40:07 -0700

    Updates

---
+++ precache-manifest.ed4a828cbffe509d584177d21545be22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c807c2f89c9ce543983caa34e414e3",
+    "url": "/qrcode/index.html"
+  },
+  {
+    "revision": "08af7ba8f9b42f71f0a3",
+    "url": "/qrcode/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d36120362429602b0b62",
+    "url": "/qrcode/static/js/2.2a6da68b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qrcode/static/js/2.2a6da68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08af7ba8f9b42f71f0a3",
+    "url": "/qrcode/static/js/main.d098ba05.chunk.js"
+  },
+  {
+    "revision": "b6b7150212877ea6b718",
+    "url": "/qrcode/static/js/runtime-main.f231d52f.js"
+  }
+]);
